36bf169fa72e7948ce83ae5be033f14995f9bb71
Andres Fernando Corani Rodriguez <Andre@MacBook-Pro-de-Andres-Corani.local>
2024-07-26 15:30:06 -0400

    4 commit para produccion

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': process.env.PORT || 5173
-  }
-})
+  server: {
+    host: true, // Escucha en todas las interfaces de red
+    port: process.env.PORT || 5173, // Usa el puerto proporcionado por Railway o 5173 como fallback
+  },
+  preview: {
+    host: true, // Escucha en todas las interfaces de red
+    port: process.env.PORT || 5173, // Usa el puerto proporcionado por Railway o 5173 como fallback
+  },
+});
